a80b3793fd137e912128b8b769dcbce6b4eb3dd3
leo-tarasyuk <leonid.tarasyuk52@gmail.com>
2020-09-08 21:57:17 +0300

    Updates

---
+++ precache-manifest.6a0f9aeb07356c7321b4e915f087c2a0.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfc78921d88e49f598681ec6c9bfec3",
+    "url": "/corex/index.html"
+  },
+  {
+    "revision": "30b9f2d8562f1bf32eb7",
+    "url": "/corex/static/css/2.155cf648.chunk.css"
+  },
+  {
+    "revision": "92804b7824e8526dfc44",
+    "url": "/corex/static/css/main.e9a9ebe1.chunk.css"
+  },
+  {
+    "revision": "30b9f2d8562f1bf32eb7",
+    "url": "/corex/static/js/2.838eb424.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corex/static/js/2.838eb424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92804b7824e8526dfc44",
+    "url": "/corex/static/js/main.c8f400f2.chunk.js"
+  },
+  {
+    "revision": "350f2b8bbfd0c34d1b29",
+    "url": "/corex/static/js/runtime-main.b5dbe4a7.js"
+  },
+  {
+    "revision": "c873ec807aaaab8bccd7f21bfcb065dc",
+    "url": "/corex/static/media/exo-latin-100.c873ec80.woff"
+  },
+  {
+    "revision": "ecfa429b58bed33232a5d062fc5e21bd",
+    "url": "/corex/static/media/exo-latin-100.ecfa429b.woff2"
+  },
+  {
+    "revision": "77b5c9ffdacf67636bc16331b7bcb07a",
+    "url": "/corex/static/media/exo-latin-100italic.77b5c9ff.woff"
+  },
+  {
+    "revision": "f72731347a68ec981cd6e94b9bbcb4be",
+    "url": "/corex/static/media/exo-latin-100italic.f7273134.woff2"
+  },
+  {
+    "revision": "b0cae8faf4179e2a4b8696a59d9b64cb",
+    "url": "/corex/static/media/exo-latin-200.b0cae8fa.woff"
+  },
+  {
+    "revision": "cff6706d535d3117fd37cee04185f041",
+    "url": "/corex/static/media/exo-latin-200.cff6706d.woff2"
+  },
+  {
+    "revision": "35f2aaf3897a78e44f509eb2621d7016",
+    "url": "/corex/static/media/exo-latin-200italic.35f2aaf3.woff2"
+  },
+  {
+    "revision": "449ea98cc15841f8ecf2c6bad4c74385",
+    "url": "/corex/static/media/exo-latin-200italic.449ea98c.woff"
+  },
+  {
+    "revision": "c70b6a46a9f4827c938309277035fdcc",
+    "url": "/corex/static/media/exo-latin-300.c70b6a46.woff2"
+  },
+  {
+    "revision": "dd61b520a0cfa8c7814fcdaa56664c94",
+    "url": "/corex/static/media/exo-latin-300.dd61b520.woff"
+  },
+  {
+    "revision": "48d581bbfa9bad0d7f0ab5da925c5282",
+    "url": "/corex/static/media/exo-latin-300italic.48d581bb.woff"
+  },
+  {
+    "revision": "f3611345768ec8cf65df17fcd513e623",
+    "url": "/corex/static/media/exo-latin-300italic.f3611345.woff2"
+  },
+  {
+    "revision": "55f2b78c0499a64910d2e9cc99c8ac3a",
+    "url": "/corex/static/media/exo-latin-400.55f2b78c.woff2"
+  },
+  {
+    "revision": "ff41113437b1c35e902f2202e3d9ef49",
+    "url": "/corex/static/media/exo-latin-400.ff411134.woff"
+  },
+  {
+    "revision": "6a867ffde1405e59ed30b3f3a63807e8",
+    "url": "/corex/static/media/exo-latin-400italic.6a867ffd.woff"
+  },
+  {
+    "revision": "dfdb8a3294d049f6b57c8cfd80c0c369",
+    "url": "/corex/static/media/exo-latin-400italic.dfdb8a32.woff2"
+  },
+  {
+    "revision": "3836d57e9f2d9af29e40a4ad370a8354",
+    "url": "/corex/static/media/exo-latin-500.3836d57e.woff"
+  },
+  {
+    "revision": "7a9798aef3b6497dc9536e64fca45b82",
+    "url": "/corex/static/media/exo-latin-500.7a9798ae.woff2"
+  },
+  {
+    "revision": "61c374098271d7ad7dea5b5db8b3115a",
+    "url": "/corex/static/media/exo-latin-500italic.61c37409.woff2"
+  },
+  {
+    "revision": "ec8eb4db254917427645f8e61434f895",
+    "url": "/corex/static/media/exo-latin-500italic.ec8eb4db.woff"
+  },
+  {
+    "revision": "69e3aab292ddcca9c29b72ba8654fd26",
+    "url": "/corex/static/media/exo-latin-600.69e3aab2.woff"
+  },
+  {
+    "revision": "d0279473ef00167906c98e4184c6ad74",
+    "url": "/corex/static/media/exo-latin-600.d0279473.woff2"
+  },
+  {
+    "revision": "4786cbe2462691f1c81da0bdcc1eb09c",
+    "url": "/corex/static/media/exo-latin-600italic.4786cbe2.woff2"
+  },
+  {
+    "revision": "cf237fdbd9c5599cbaf0d878dbb47098",
+    "url": "/corex/static/media/exo-latin-600italic.cf237fdb.woff"
+  },
+  {
+    "revision": "9ea5eea698aca04309d03b28d3645bd0",
+    "url": "/corex/static/media/exo-latin-700.9ea5eea6.woff"
+  },
+  {
+    "revision": "c5d6f8973998d384c2025face179b394",
+    "url": "/corex/static/media/exo-latin-700.c5d6f897.woff2"
+  },
+  {
+    "revision": "22c091983f061e4c00acacbcc6342008",
+    "url": "/corex/static/media/exo-latin-700italic.22c09198.woff"
+  },
+  {
+    "revision": "2717f2e506100c5474f182b1ecc9780b",
+    "url": "/corex/static/media/exo-latin-700italic.2717f2e5.woff2"
+  },
+  {
+    "revision": "5c140b49a21057618a2ebb10d4a7d6a0",
+    "url": "/corex/static/media/exo-latin-800.5c140b49.woff"
+  },
+  {
+    "revision": "dd49577c7dd8bafe905b6e69fa5c1e8c",
+    "url": "/corex/static/media/exo-latin-800.dd49577c.woff2"
+  },
+  {
+    "revision": "1d4c476b4b8180eb35ab3c6758950c8d",
+    "url": "/corex/static/media/exo-latin-800italic.1d4c476b.woff2"
+  },
+  {
+    "revision": "4f399c28972b1825a4b9fd914551f1e5",
+    "url": "/corex/static/media/exo-latin-800italic.4f399c28.woff"
+  },
+  {
+    "revision": "37a470710e160b4cc6d46abe21989713",
+    "url": "/corex/static/media/exo-latin-900.37a47071.woff"
+  },
+  {
+    "revision": "fbe4caf50ede705b85e51a7e35ee4594",
+    "url": "/corex/static/media/exo-latin-900.fbe4caf5.woff2"
+  },
+  {
+    "revision": "3aafcbed4a86d24c925def38ff1522ea",
+    "url": "/corex/static/media/exo-latin-900italic.3aafcbed.woff2"
+  },
+  {
+    "revision": "a17b302ec683c2a7a1713d7a382d300d",
+    "url": "/corex/static/media/exo-latin-900italic.a17b302e.woff"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/corex/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/corex/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/corex/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/corex/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/corex/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/corex/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/corex/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/corex/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/corex/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/corex/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/corex/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/corex/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/corex/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/corex/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/corex/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/corex/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/corex/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/corex/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/corex/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/corex/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  }
+]);
